fd528040f57a497410bdc4b1e1c4d1c066bcbd74
Jeff Seymour <seym1585@gmail.com>
2015-10-16 16:19:37 -0700

    add javascript for ping-pong loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
 
+
   $("header").slideDown("slow", function() {
-    $(".question").fadeIn("slow");  
+    $(".question").fadeIn("slow");
   });
+
+
+
+  $(".btn").click(function(event) {
+      var userInput = $("#numInput").val();
+
+    for (var index = 1; index <= userInput; index ++) {
+      if (index % 15 === 0) {
+        $(".numbers").append("<p>pingpong</p>");
+      } else if (index % 5 === 0) {
+        $(".numbers").append("<p>pong</p>");
+      } else if (index % 3 === 0) {
+        $(".numbers").append("<p>ping</p>");
+      } else {
+        $(".numbers").append("<p>" + index + "</p>");
+      };
+
+    }
+
+
+
+
+    event.preventDefault();
+  })
 
 
 
