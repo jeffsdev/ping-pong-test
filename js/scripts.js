886c9ac5d226db83d4c844f37da650310537d563
Jeff Seymour <seym1585@gmail.com>
2015-10-16 17:01:15 -0700

    add additional functionality to scripts.js, and more css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,28 @@
 $(document).ready(function() {
 
-
   $("header").slideDown("slow", function() {
     $(".question").fadeIn("slow");
   });
 
-
-
   $(".btn").click(function(event) {
-      var userInput = $("#numInput").val();
-
+    // get number from user //
+    var userInput = $("#numInput").val();
+    // remove any previous content in .numbers div //
+    $(".numbers").text("");
+    $(".numbers").fadeIn("slow");
+    // ping-pong loop //
     for (var index = 1; index <= userInput; index ++) {
       if (index % 15 === 0) {
-        $(".numbers").append("<p>pingpong</p>");
+        $(".numbers").append("<p class='pp'>ping-pong</p>");
       } else if (index % 5 === 0) {
-        $(".numbers").append("<p>pong</p>");
+        $(".numbers").append("<p class='po'>pong</p>");
       } else if (index % 3 === 0) {
-        $(".numbers").append("<p>ping</p>");
+        $(".numbers").append("<p class='pi'>ping</p>");
       } else {
         $(".numbers").append("<p>" + index + "</p>");
       };
-
-    }
-
-
-
-
+    };
     event.preventDefault();
-  })
-
-
-
-
+  });
 
 });
